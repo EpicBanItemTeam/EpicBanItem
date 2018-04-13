d465f6e919046f19f47b0e958f3a04b518a0281a
yinyangshi <1418780411@qq.com>
2018-04-13 22:38:23 +0800

    init

--- src/main/java/com/github/euonmyoji/epicbanitem/EpicBanItem.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/EpicBanItem.java
@@ -1,7 +1,10 @@
 package com.github.euonmyoji.epicbanitem;
 
 import com.github.euonmyoji.epicbanitem.listeners.GetItemListener;
+import com.github.euonmyoji.epicbanitem.listeners.SummonListener;
 import com.github.euonmyoji.epicbanitem.listeners.WorldItemMoveListener;
+import com.google.inject.Inject;
+import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.event.Listener;
@@ -9,17 +12,20 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStartingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-import javax.inject.Inject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Plugin(id = "epicbanitem", name = "EpicBanItem", version = "1.0", authors = {"yinyangshi", "GINYAI", "ustc-zzzz"})
+@Plugin(id = "epicbanitem", name = "EpicBanItem", version = EpicBanItem.VERSION, authors = {"yinyangshi", "GINYAI", "ustc-zzzz"})
 public class EpicBanItem {
+    static final String VERSION = "1.0";
 
     @Inject
     @ConfigDir(sharedRoot = false)
     public static Path cfgDir;
+
+    @Inject
+    public Logger logger;
 
     @Listener
     public void onStarting(GameStartingServerEvent event) {
@@ -36,5 +42,6 @@ public class EpicBanItem {
     public void onStarted(GameStartedServerEvent event) {
         Sponge.getEventManager().registerListeners(new GetItemListener(), this);
         Sponge.getEventManager().registerListeners(new WorldItemMoveListener(), this);
+        Sponge.getEventManager().registerListeners(new SummonListener(), this);
     }
 }
