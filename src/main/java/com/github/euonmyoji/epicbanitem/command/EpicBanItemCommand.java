4e35e1b45925e3d1b97e7648472dae469c7af29e
yinyangshi <1418780411@qq.com>
2018-04-15 21:42:50 +0800

    add CheckResult class and add some methods in TextUtil

--- src/main/java/com/github/euonmyoji/epicbanitem/command/EpicBanItemCommand.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/command/EpicBanItemCommand.java
@@ -3,8 +3,8 @@ package com.github.euonmyoji.epicbanitem.command;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.spec.CommandSpec;
 
-import static com.github.euonmyoji.epicbanitem.util.TextUtil.runCommandGui;
-import static com.github.euonmyoji.epicbanitem.util.TextUtil.suggestCommandGui;
+import static com.github.euonmyoji.epicbanitem.util.TextUtil.runCommandGuiThenSend;
+import static com.github.euonmyoji.epicbanitem.util.TextUtil.suggestCommandGuiThenSend;
 
 /**
  * @author 主yinyangshi
@@ -14,19 +14,19 @@ public class EpicBanItemCommand {
     public static CommandSpec ebi = CommandSpec.builder()
             .permission("epicbanitem.epicbanitem")
             .executor((src, args) -> {
-                src.sendMessage(runCommandGui("/ebi version", "(v)查看插件版本"));
-                src.sendMessage(runCommandGui("/ebi reload", "(r)重载插件"));
-                src.sendMessage(runCommandGui("/ebi list", "列出手中物品下的所有可用的规则"));
-                src.sendMessage(runCommandGui("/ebi list --all", "列出所有可用的规则"));
-                src.sendMessage(suggestCommandGui("/ebi list --item", "列出同一个物品下的所有可用的规则", "<item-type>"));
-                src.sendMessage(suggestCommandGui("/ebi query", "使用特定规则检查手中物品，并列出nbt等信息", "<query-rule>"));
-                src.sendMessage(suggestCommandGui("/ebi show", "展示手中物品的特定的规则", "<rule-name>"));
-                src.sendMessage(suggestCommandGui("/ebi show --item", "展示某个特定的规则", "<item-type> <rule-name>"));
-                src.sendMessage(suggestCommandGui("/ebi create", "添加手中物品的某个规则", "<rule-name>"));
-                src.sendMessage(suggestCommandGui("/ebi update", "设置手中物品规则的某些值", "<rule-name>"));
-                src.sendMessage(suggestCommandGui("/ebi update --item", "设置某个规则的某些值", "<item-type> <rule-name>"));
-                src.sendMessage(runCommandGui("/ebi apply", "将所有可用的规则全部应用到手中的物品上"));
-                src.sendMessage(suggestCommandGui("/ebi apply", "将某规则应用到手中的物品上", "<rule-name>"));
+                runCommandGuiThenSend(src, "/ebi version", "(v)查看插件版本");
+                runCommandGuiThenSend(src, "/ebi reload", "(r)重载插件");
+                runCommandGuiThenSend(src, "/ebi list", "列出手中物品下的所有可用的规则");
+                runCommandGuiThenSend(src, "/ebi list --all", "列出所有可用的规则");
+                suggestCommandGuiThenSend(src, "/ebi list --item", "列出同一个物品下的所有可用的规则", "<item-type>");
+                suggestCommandGuiThenSend(src, "/ebi query", "使用特定规则检查手中物品，并列出nbt等信息", "<query-rule>");
+                suggestCommandGuiThenSend(src, "/ebi show", "展示手中物品的特定的规则", "<rule-name>");
+                suggestCommandGuiThenSend(src, "/ebi show --item", "展示某个特定的规则", "<item-type> <rule-name>");
+                suggestCommandGuiThenSend(src, "/ebi create", "添加手中物品的某个规则", "<rule-name>");
+                suggestCommandGuiThenSend(src, "/ebi update", "设置手中物品规则的某些值", "<rule-name>");
+                suggestCommandGuiThenSend(src, "/ebi update --item", "设置某个规则的某些值", "<item-type> <rule-name>");
+                runCommandGuiThenSend(src, "/ebi apply", "将所有可用的规则全部应用到手中的物品上");
+                suggestCommandGuiThenSend(src, "/ebi apply", "将某规则应用到手中的物品上", "<rule-name>");
                 return CommandResult.success();
             })
             .child(Apply.apply, "apply", "a")
--- src/main/java/com/github/euonmyoji/epicbanitem/util/TextUtil.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/util/TextUtil.java
@@ -1,6 +1,7 @@
 package com.github.euonmyoji.epicbanitem.util;
 
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageReceiver;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -17,7 +18,7 @@ public class TextUtil {
      * @param hoverStr 覆盖时的text 如果没有就用默认的
      * @return 带gui的指令提示text
      */
-    public static Text runCommandGui(String command, String describe, @Nullable String hoverStr) {
+    private static Text runCommandGui(String command, String describe, @Nullable String hoverStr) {
         return builder().append(of(command + " " + describe))
                 .onClick(runCommand(command))
                 .onHover(showText(of(hoverStr != null ? hoverStr : "点击执行" + command)))
@@ -29,15 +30,35 @@ public class TextUtil {
     }
 
     /**
+     * @param receiver 接受gui消息的人
+     * @see TextUtil
+     */
+    public static void runCommandGuiThenSend(MessageReceiver receiver, String command, String describe, @Nullable String hoverStr) {
+        receiver.sendMessage(runCommandGui(command, describe, hoverStr));
+    }
+
+    public static void runCommandGuiThenSend(MessageReceiver receiver, String command, String describe) {
+        runCommandGuiThenSend(receiver, command, describe, null);
+    }
+
+    /**
      * @param command     指令#点击后显示的 带/
      * @param describe    指令描述
      * @param commandArgs 命令所需参数
      * @return 带gui的指令提示text
      */
-    public static Text suggestCommandGui(String command, String describe, @Nonnull String commandArgs) {
+    private static Text suggestCommandGui(String command, String describe, @Nonnull String commandArgs) {
         return builder().append(of(command + " " + commandArgs + " " + describe))
                 .onClick(suggestCommand(command + " "))
                 .onHover(showText(of(command + " " + commandArgs)))
                 .build();
+    }
+
+    /**
+     * @param receiver 接受gui消息的人
+     * @see TextUtil
+     */
+    public static void suggestCommandGuiThenSend(MessageReceiver receiver, String command, String describe, @Nonnull String commandArgs) {
+        receiver.sendMessage(suggestCommandGui(command, describe, commandArgs));
     }
 }
