3610afb94498c2dcc8ad8eb1f50a450007d88d38
yinyangshi <1418780411@qq.com>
2018-08-08 14:12:32 +0800

    do u mean... ?

--- src/main/java/com/github/euonmyoji/epicbanitem/command/CommandEbi.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/command/CommandEbi.java
@@ -2,7 +2,6 @@ package com.github.euonmyoji.epicbanitem.command;
 
 import com.github.euonmyoji.epicbanitem.EpicBanItem;
 import org.spongepowered.api.command.CommandCallable;
-import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
@@ -17,6 +16,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author EBI
+ */
 @NonnullByDefault
 public class CommandEbi extends AbstractCommand {
 
@@ -46,15 +48,38 @@ public class CommandEbi extends AbstractCommand {
                 .build();
     }
 
+    private static final String ARGUMENT_KEY = "string";
+
     @Override
     public CommandElement getArgument() {
-        return GenericArguments.optionalWeak(GenericArguments.remainingJoinedStrings(Text.of("string")));
+        return GenericArguments.optionalWeak(GenericArguments.remainingJoinedStrings(Text.of(ARGUMENT_KEY)));
     }
 
     @Override
-    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+    public CommandResult execute(CommandSource src, CommandContext args) {
         src.sendMessage(getMessage("version", "version", EpicBanItem.VERSION));
         src.sendMessage(getMessage("useHelp", "help_command", "/" + EpicBanItem.plugin.getMainCommandAlias() + " help"));
+        args.<String>getOne(ARGUMENT_KEY).ifPresent(s -> {
+            String lastMatchCommand = null;
+            int lastM = -1;
+            for (Map.Entry<List<String>, CommandCallable> entry : childrenMap.entrySet()) {
+                if (entry.getValue().testPermission(src)) {
+                    try {
+                        String command = entry.getKey().get(0);
+                        int d = getHowClose(command, s);
+                        if (d > lastM) {
+                            lastMatchCommand = command;
+                            lastM = d;
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        EpicBanItem.logger.debug("未知IndexOut情况", e);
+                    }
+                }
+            }
+            if (lastMatchCommand != null) {
+                src.sendMessage(Text.of(String.format("Do you mean /ebi %s?", lastMatchCommand)));
+            }
+        });
         return CommandResult.success();
     }
 
@@ -62,4 +87,30 @@ public class CommandEbi extends AbstractCommand {
         childrenMap.put(command.getNameList(), command.getCallable());
     }
 
+
+    private static int getHowClose(String raw, String s) {
+        //s长度不大于raw的1.5倍并且不匹配的字符不超过raw字符数 并且至少有一半字符匹配raw 才算接近
+        final float offset = 1.5f;
+        final int rawLen = raw.length();
+        int d = 0;
+        if (rawLen * offset >= s.length()) {
+            for (int i = 0; i < s.length(); i++) {
+
+                if (s.contains(raw)) {
+                    return rawLen + d;
+                }
+
+                String c = s.substring(i, i + 1);
+                if (raw.contains(c)) {
+                    d++;
+                    raw = raw.replaceFirst(c, "");
+                } else {
+                    d--;
+                }
+            }
+        } else {
+            return Integer.MIN_VALUE;
+        }
+        return d;
+    }
 }
--- src/main/java/com/github/euonmyoji/epicbanitem/util/TextUtil.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/util/TextUtil.java
@@ -11,12 +11,8 @@ import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.spongepowered.api.data.DataView;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.TextTemplate;
-import org.spongepowered.api.text.channel.MessageReceiver;
-import org.spongepowered.api.text.format.TextStyles;
 import org.spongepowered.api.text.serializer.TextSerializers;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -24,65 +20,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import static org.spongepowered.api.text.Text.builder;
-import static org.spongepowered.api.text.Text.of;
-import static org.spongepowered.api.text.action.TextActions.*;
-
 /**
  * @author yinyangshi
  */
-@SuppressWarnings("WeakerAccess")
 public class TextUtil {
 
-    /**
-     * @param command  指令#点击后执行的 带/
-     * @param describe 指令描述
-     * @param hoverStr 覆盖时的text 如果没有就用默认的
-     * @return 带gui的指令提示text
-     */
-    private static Text runCommandGui(String command, String describe, @Nullable String hoverStr) {
-        return builder().append(of(TextStyles.UNDERLINE, command, TextStyles.RESET, " " + describe))
-                .onClick(runCommand(command))
-                .onHover(showText(of(hoverStr != null ? hoverStr : "点击执行" + command)))
-                .build();
-    }
-
-    public static Text runCommandGui(String command, String describe) {
-        return runCommandGui(command, describe, null);
-    }
-
-    /**
-     * @param receiver 接受gui消息的人
-     * @see TextUtil
-     */
-    public static void runCommandGuiThenSend(MessageReceiver receiver, String command, String describe, @Nullable String hoverStr) {
-        receiver.sendMessage(runCommandGui(command, describe, hoverStr));
-    }
-
-    public static void runCommandGuiThenSend(MessageReceiver receiver, String command, String describe) {
-        runCommandGuiThenSend(receiver, command, describe, null);
-    }
-
-    /**
-     * @param command     指令#点击后显示的 带/
-     * @param describe    指令描述
-     * @param commandArgs 命令所需参数
-     * @return 带gui的指令提示text
-     */
-    private static Text suggestCommandGui(String command, String describe, @Nonnull String commandArgs) {
-        return builder().append(of(TextStyles.UNDERLINE, command + " " + commandArgs, TextStyles.RESET, " " + describe))
-                .onClick(suggestCommand(command + " "))
-                .onHover(showText(of(command + " " + commandArgs)))
-                .build();
-    }
-
-    /**
-     * @param receiver 接受gui消息的人
-     * @see TextUtil
-     */
-    public static void suggestCommandGuiThenSend(MessageReceiver receiver, String command, String describe, @Nonnull String commandArgs) {
-        receiver.sendMessage(suggestCommandGui(command, describe, commandArgs));
-    }
 
     /**
      * @param origin origin string support FormatText
@@ -122,6 +64,7 @@ public class TextUtil {
         return TextTemplate.of(objects.toArray());
     }
 
+
     public static Text parseFormatText(String in) {
         return TextSerializers.FORMATTING_CODE.deserializeUnchecked(in);
     }
@@ -155,7 +98,7 @@ public class TextUtil {
                 .setSource(() -> delegationReader).setSink(() -> delegationWriter);
         try {
             for (Method method : HoconConfigurationLoader.Builder.class.getMethods()) {
-                if (method.getName().equals("setParseOptions")) {
+                if ("setParseOptions".equals(method.getName())) {
                     Class<?> parseOptionsClass = method.getParameterTypes()[0];
                     Method setAllowMissingMethod = parseOptionsClass.getMethod("setAllowMissing", boolean.class);
                     Object defaultParseOptions = parseOptionsClass.getMethod("defaults").invoke(null);
