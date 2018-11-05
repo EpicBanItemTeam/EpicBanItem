3398cfe9c824b0fa2abe21e801f15f1ab0150de5
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2018-11-06 02:08:46 +0800

    Show tile entity data even though there is not any corresponding item for the block state

--- src/main/java/com/github/euonmyoji/epicbanitem/util/NbtTagDataUtil.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/util/NbtTagDataUtil.java
@@ -18,7 +18,9 @@ import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.util.Tuple;
 
-import java.util.*;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -26,6 +28,21 @@ import java.util.stream.Stream;
  * @author ustc_zzzz
  */
 public class NbtTagDataUtil {
+    private static final DataQuery ID = DataQuery.of("id");
+    private static final DataQuery TAG = DataQuery.of("tag");
+    private static final DataQuery DAMAGE = DataQuery.of("Damage");
+    private static final DataQuery BLOCK_ENTITY_TAG = DataQuery.of("tag", "BlockEntityTag");
+    private static final DataQuery CUSTOM_MANIPULATORS = DataQuery.of("tag", "SpongeData", "CustomManipulators");
+
+    private static final DataQuery DATA = DataQuery.of("Data");
+    private static final DataQuery COUNT = DataQuery.of("Count");
+    private static final DataQuery POSITION = DataQuery.of("Position");
+    private static final DataQuery ITEM_TYPE = DataQuery.of("ItemType");
+    private static final DataQuery WORLD_UUID = DataQuery.of("WorldUuid");
+    private static final DataQuery UNSAFE_DATA = DataQuery.of("UnsafeData");
+    private static final DataQuery BLOCK_STATE = DataQuery.of("BlockState");
+    private static final DataQuery UNSAFE_DAMAGE = DataQuery.of("UnsafeDamage");
+
     public static void printToLogger(Consumer<String> logger) {
         logger.accept("Generating Item to Block mapping: ");
         Map.ITEM_TO_BLOCK.forEach((k, v) -> {
@@ -36,14 +53,17 @@ public class NbtTagDataUtil {
     }
 
     public static DataContainer toNbt(BlockSnapshot snapshot) {
-        boolean hasItem = snapshot.getState().getType().getItem().isPresent();
-        ItemStack stack = hasItem ? ItemStack.builder().fromBlockSnapshot(snapshot).build() : ItemStack.empty();
-        DataContainer container = fromSpongeDataToNbt(stack.toContainer());
         Vector3i position = snapshot.getPosition();
-        container.set(DataQuery.of("tag", "BlockEntityTag", "x"), position.getX());
-        container.set(DataQuery.of("tag", "BlockEntityTag", "y"), position.getY());
-        container.set(DataQuery.of("tag", "BlockEntityTag", "z"), position.getZ());
-        return container;
+
+        DataContainer result = fromSpongeDataToNbt(Map.getItemByBlock(snapshot.getState()).orElseGet(() -> ItemStack.empty().toContainer()));
+
+        snapshot.toContainer().getView(UNSAFE_DATA).ifPresent(nbt -> result.set(BLOCK_ENTITY_TAG, nbt));
+
+        result.set(BLOCK_ENTITY_TAG.then("x"), position.getX());
+        result.set(BLOCK_ENTITY_TAG.then("y"), position.getY());
+        result.set(BLOCK_ENTITY_TAG.then("z"), position.getZ());
+
+        return result;
     }
 
     public static DataContainer toNbt(ItemStackSnapshot stackSnapshot) {
@@ -57,22 +77,15 @@ public class NbtTagDataUtil {
     public static BlockSnapshot toBlockSnapshot(DataView view, BlockState oldState, UUID worldUniqueId) throws InvalidDataException {
         DataContainer result = DataContainer.createNew(DataView.SafetyMode.NO_DATA_CLONED);
 
-        DataView nbt = view.getView(DataQuery.of("tag", "BlockEntityTag")).orElseThrow(InvalidDataException::new);
+        Map.getBlockByItem(view, oldState).ifPresent(state -> result.set(BLOCK_STATE, state));
 
-        result.set(DataQuery.of("Position", "X"), nbt.get(DataQuery.of("x")).orElseThrow(InvalidDataException::new));
-        result.set(DataQuery.of("Position", "Y"), nbt.get(DataQuery.of("y")).orElseThrow(InvalidDataException::new));
-        result.set(DataQuery.of("Position", "Z"), nbt.get(DataQuery.of("z")).orElseThrow(InvalidDataException::new));
+        view.get(BLOCK_ENTITY_TAG.then("x")).ifPresent(x -> result.set(POSITION.then("X"), x));
+        view.get(BLOCK_ENTITY_TAG.then("y")).ifPresent(y -> result.set(POSITION.then("Y"), y));
+        view.get(BLOCK_ENTITY_TAG.then("z")).ifPresent(z -> result.set(POSITION.then("Z"), z));
 
-        result.set(DataQuery.of("WorldUuid"), worldUniqueId.toString());
-        result.set(DataQuery.of("UnsafeData"), nbt);
+        view.get(BLOCK_ENTITY_TAG).ifPresent(nbt -> result.set(UNSAFE_DATA, nbt));
 
-        Set<BlockState> blockStates = findStatesForItemStack(view);
-
-        if (blockStates.contains(oldState)) {
-            result.set(DataQuery.of("BlockState"), oldState);
-        } else if (!blockStates.isEmpty()) {
-            result.set(DataQuery.of("BlockState"), blockStates.iterator().next());
-        }
+        result.set(WORLD_UUID, worldUniqueId.toString());
 
         return BlockSnapshot.builder().build(result).orElseThrow(InvalidDataException::new);
     }
@@ -80,13 +93,13 @@ public class NbtTagDataUtil {
     public static ItemStack toItemStack(DataView view, int stackSize) throws InvalidDataException {
         DataContainer result = DataContainer.createNew(DataView.SafetyMode.NO_DATA_CLONED);
 
-        view.get(DataQuery.of("tag", "SpongeData", "CustomManipulators")).ifPresent(data -> result.set(DataQuery.of("Data"), data));
+        view.get(CUSTOM_MANIPULATORS).ifPresent(data -> result.set(DATA, data));
 
-        view.get(DataQuery.of("id")).ifPresent(id -> result.set(DataQuery.of("ItemType"), id));
-        view.get(DataQuery.of("tag")).ifPresent(nbt -> result.set(DataQuery.of("UnsafeData"), nbt));
-        view.get(DataQuery.of("Damage")).ifPresent(damage -> result.set(DataQuery.of("UnsafeDamage"), damage));
+        view.get(ID).ifPresent(id -> result.set(ITEM_TYPE, id));
+        view.get(TAG).ifPresent(nbt -> result.set(UNSAFE_DATA, nbt));
+        view.get(DAMAGE).ifPresent(damage -> result.set(UNSAFE_DAMAGE, damage));
 
-        result.set(DataQuery.of("Count"), stackSize);
+        result.set(COUNT, stackSize);
 
         return ItemStack.builder().build(result).orElseThrow(InvalidDataException::new);
     }
@@ -94,47 +107,43 @@ public class NbtTagDataUtil {
     private static DataContainer fromSpongeDataToNbt(DataContainer view) {
         DataContainer result = DataContainer.createNew(DataView.SafetyMode.NO_DATA_CLONED);
 
-        view.get(DataQuery.of("ItemType")).ifPresent(id -> result.set(DataQuery.of("id"), id));
-        view.get(DataQuery.of("UnsafeData")).ifPresent(nbt -> result.set(DataQuery.of("tag"), nbt));
-        view.get(DataQuery.of("UnsafeDamage")).ifPresent(damage -> result.set(DataQuery.of("Damage"), damage));
+        view.get(ITEM_TYPE).ifPresent(id -> result.set(ID, id));
+        view.get(UNSAFE_DATA).ifPresent(nbt -> result.set(TAG, nbt));
+        view.get(UNSAFE_DAMAGE).ifPresent(damage -> result.set(DAMAGE, damage));
 
-        view.get(DataQuery.of("Data")).ifPresent(data -> result.set(DataQuery.of("tag", "SpongeData", "CustomManipulators"), data));
+        view.get(DATA).ifPresent(data -> result.set(CUSTOM_MANIPULATORS, data));
 
         return result;
-    }
-
-    private static Set<BlockState> findStatesForItemStack(DataView view) {
-        return Map.ITEM_TO_BLOCK.get(Tuple.of(view.get(DataQuery.of("id")), view.get(DataQuery.of("Damage"))));
     }
 
     private static final class Map {
         private static final SetMultimap<Tuple<Optional<?>, Optional<?>>, BlockState> ITEM_TO_BLOCK;
 
+        private static Optional<BlockState> getBlockByItem(DataView view, BlockState oldState) {
+            Set<BlockState> blockStates = ITEM_TO_BLOCK.get(Tuple.of(view.get(ID), view.get(DAMAGE)));
+            if (blockStates.isEmpty()) {
+                return Optional.empty();
+            } else {
+                return Optional.of(blockStates.contains(oldState) ? oldState : blockStates.iterator().next());
+            }
+        }
+
+        private static Optional<DataContainer> getItemByBlock(BlockState state) {
+            try {
+                Preconditions.checkArgument(state.getType().getItem().isPresent());
+                return Optional.of(ItemStack.builder().fromBlockState(state).build().toContainer());
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }
+
         static {
             SetMultimap<Tuple<Optional<?>, Optional<?>>, BlockState> map;
             map = MultimapBuilder.linkedHashKeys().linkedHashSetValues().build();
-            Collection<BlockType> blocks = Sponge.getRegistry().getAllOf(BlockType.class);
-            List<BlockState> statesWithoutCorrespondingItems = new ArrayList<>(blocks.size() * 16);
-            statesWithoutCorrespondingItems.add(BlockTypes.AIR.getDefaultState()); // air first
-            for (BlockType type : blocks) {
-                boolean hasItem = type.getItem().isPresent();
-                Stream.concat(Stream.of(type.getDefaultState()), type.getAllBlockStates().stream()).forEach(state -> {
-                    try {
-                        Preconditions.checkArgument(hasItem);
-                        DataContainer nbt = ItemStack.builder().fromBlockState(state).build().toContainer();
-                        Optional<Object> damage = nbt.get(DataQuery.of("UnsafeDamage"));
-                        Optional<Object> id = nbt.get(DataQuery.of("ItemType"));
-                        map.put(Tuple.of(id, damage), state);
-                    } catch (Exception e) {
-                        statesWithoutCorrespondingItems.add(state);
-                    }
-                });
-            }
-            DataContainer nbt = ItemStack.empty().toContainer();
-            statesWithoutCorrespondingItems.forEach(state -> {
-                Optional<Object> damage = nbt.get(DataQuery.of("UnsafeDamage"));
-                Optional<Object> id = nbt.get(DataQuery.of("ItemType"));
-                map.put(Tuple.of(id, damage), state);
+            Stream.concat(BlockTypes.AIR.getAllBlockStates().stream(), Sponge.getRegistry().getAllOf(BlockType.class).stream()
+                    .flatMap(type -> Stream.concat(Stream.of(type.getDefaultState()), type.getAllBlockStates().stream()))).forEach(state -> {
+                DataContainer nbt = getItemByBlock(state).orElseGet(() -> ItemStack.empty().toContainer());
+                map.put(Tuple.of(nbt.get(ITEM_TYPE), nbt.get(UNSAFE_DAMAGE)), state);
             });
             ITEM_TO_BLOCK = Multimaps.unmodifiableSetMultimap(map);
         }
