3c232462d995c762cca35b18ff0361982b8dabbe
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2019-01-13 20:28:07 +0800

    Revert "main" since it is uncompleted
    This reverts commit 12a89e866f7f92b49acbd9c59f557f3575fc5e0d.

--- src/main/java/com/github/euonmyoji/epicbanitem/EpicBanItem.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/EpicBanItem.java
@@ -30,10 +30,9 @@ import java.util.Optional;
 /**
  * @author yinyangshi GiNYAi ustc_zzzz
  */
-@Plugin(id = "epicbanitem", name = "EpicBanItem", version = EpicBanItem.VERSION,
+@Plugin(id = "epicbanitem", name = "EpicBanItem", version = "@version@",
         authors = {"yinyangshi", "GiNYAi", "ustc-zzzz"}, description = "A banitem with nbt plugin in sponge")
 public class EpicBanItem {
-    static final String VERSION = "@version@";
     private static EpicBanItem instance;
 
     private final Path cfgDir;
