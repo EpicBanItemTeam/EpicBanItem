b760a2054cd861bd52cf782b4007d2c7fbe99a6e
SettingDust <settingdust@gmail.com>
2020-02-12 19:21:01 +0800

    :sparkles: Add check after edit & after join

--- src/main/java/com/github/euonmyoji/epicbanitem/check/listener/InventoryListener.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/check/listener/InventoryListener.java
@@ -1,5 +1,7 @@
 package com.github.euonmyoji.epicbanitem.check.listener;
 
+import static com.github.euonmyoji.epicbanitem.util.TextUtil.getDisplayName;
+
 import com.github.euonmyoji.epicbanitem.EpicBanItem;
 import com.github.euonmyoji.epicbanitem.api.CheckResult;
 import com.github.euonmyoji.epicbanitem.api.CheckRuleTrigger;
@@ -7,6 +9,9 @@ import com.github.euonmyoji.epicbanitem.check.CheckRuleService;
 import com.github.euonmyoji.epicbanitem.check.Triggers;
 import com.github.euonmyoji.epicbanitem.util.NbtTagDataUtil;
 import com.github.euonmyoji.epicbanitem.util.TextUtil;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.data.DataContainer;
@@ -23,30 +28,28 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.entity.ChangeEntityEquipmentEvent;
 import org.spongepowered.api.event.entity.living.humanoid.HandInteractEvent;
+import org.spongepowered.api.event.filter.IsCancelled;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.filter.type.Exclude;
 import org.spongepowered.api.event.filter.type.Include;
 import org.spongepowered.api.event.item.inventory.*;
+import org.spongepowered.api.event.network.ClientConnectionEvent.Join;
 import org.spongepowered.api.item.inventory.*;
 import org.spongepowered.api.item.inventory.equipment.WornEquipmentType;
 import org.spongepowered.api.item.inventory.property.EquipmentSlotType;
 import org.spongepowered.api.item.inventory.transaction.SlotTransaction;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.util.Tristate;
 import org.spongepowered.api.world.BlockChangeFlags;
 import org.spongepowered.api.world.Locatable;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.storage.WorldProperties;
 
-import java.util.*;
-import java.util.stream.Stream;
-
-import static com.github.euonmyoji.epicbanitem.util.TextUtil.getDisplayName;
-
 /**
  * @author yinyangshi GiNYAi ustc_zzzz
  */
+@SuppressWarnings("DuplicatedCode")
 public class InventoryListener {
-
     private CheckRuleService service = Sponge.getServiceManager().provideUnchecked(CheckRuleService.class);
 
     private static ItemStack getItem(DataContainer view, int quantity) {
@@ -60,13 +63,19 @@ public class InventoryListener {
             CheckResult result = service.check(item, player.getWorld(), Triggers.THROW, player);
             if (result.isBanned()) {
                 event.setCancelled(true);
-                Optional<ItemStack> optionalFinalItem = result.getFinalView()
-                        .map(view -> getItem(view, item.getQuantity()));
+                Optional<ItemStack> optionalFinalItem = result.getFinalView().map(view -> getItem(view, item.getQuantity()));
                 optionalFinalItem.ifPresent(finalItem -> tran.setCustom(finalItem.createSnapshot()));
                 Text originItemName = getDisplayName(item.createStack());
                 Text finalItemName = getDisplayName(optionalFinalItem.orElse(item.createStack()));
-                TextUtil.prepareMessage(Triggers.THROW, originItemName, finalItemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
-                        .forEach(player::sendMessage);
+                TextUtil
+                    .prepareMessage(
+                        Triggers.THROW,
+                        originItemName,
+                        finalItemName,
+                        ((CheckResult.Banned) result).getBanRules(),
+                        result.isUpdateNeeded()
+                    )
+                    .forEach(player::sendMessage);
             }
         }
     }
@@ -85,8 +94,15 @@ public class InventoryListener {
                 if (player != null) {
                     Text originItemName = getDisplayName(item.createStack());
                     Text finalItemName = getDisplayName(optionalFinalItem.orElse(item.createStack()));
-                    TextUtil.prepareMessage(Triggers.DROP, originItemName, finalItemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
-                            .forEach(player::sendMessage);
+                    TextUtil
+                        .prepareMessage(
+                            Triggers.DROP,
+                            originItemName,
+                            finalItemName,
+                            ((CheckResult.Banned) result).getBanRules(),
+                            result.isUpdateNeeded()
+                        )
+                        .forEach(player::sendMessage);
                 }
             }
         }
@@ -108,14 +124,21 @@ public class InventoryListener {
                 }
             }
             if (worlds.isEmpty()) {
-                worlds.add(cause.first(Locatable.class).map(Locatable::getWorld).orElseGet(() -> {
-                    RuntimeException e = new RuntimeException("EpicBanItem cannot even find a world when crafting");
-                    WorldProperties defProps = Sponge.getServer().getDefaultWorld().orElseThrow(RuntimeException::new);
-                    World def = Sponge.getServer().getWorld(defProps.getUniqueId()).orElseThrow(RuntimeException::new);
-                    EpicBanItem.getLogger().warn("EpicBanItem cannot even find a world! What is the server doing?");
-                    EpicBanItem.getLogger().debug("No world found in " + cause.toString(), e);
-                    return def;
-                }));
+                worlds.add(
+                    cause
+                        .first(Locatable.class)
+                        .map(Locatable::getWorld)
+                        .orElseGet(
+                            () -> {
+                                RuntimeException e = new RuntimeException("EpicBanItem cannot even find a world when crafting");
+                                WorldProperties defProps = Sponge.getServer().getDefaultWorld().orElseThrow(RuntimeException::new);
+                                World def = Sponge.getServer().getWorld(defProps.getUniqueId()).orElseThrow(RuntimeException::new);
+                                EpicBanItem.getLogger().warn("EpicBanItem cannot even find a world! What is the server doing?");
+                                EpicBanItem.getLogger().debug("No world found in " + cause.toString(), e);
+                                return def;
+                            }
+                        )
+                );
             }
             for (Transaction<ItemStackSnapshot> transaction : event.getTransactions()) {
                 ItemStackSnapshot item = transaction.getFinal();
@@ -134,7 +157,7 @@ public class InventoryListener {
     }
 
     @Listener(order = Order.FIRST, beforeModifications = true)
-    @Exclude({ClickInventoryEvent.Drop.class})
+    @Exclude({ ClickInventoryEvent.Drop.class })
     public void onClicked(ClickInventoryEvent event, @First Player player) {
         CheckRuleTrigger trigger = Triggers.CLICK;
         Stream<SlotTransaction> slotTransactionStream = event.getTransactions().stream();
@@ -155,8 +178,9 @@ public class InventoryListener {
             optionalFinalItem.ifPresent(finalItem -> itemEntity.offer(Keys.REPRESENTED_ITEM, finalItem.createSnapshot()));
             Text originItemName = getDisplayName(itemEntity);
             Text finalItemName = getDisplayName(optionalFinalItem.orElse(item.createStack()));
-            TextUtil.prepareMessage(Triggers.PICKUP, originItemName, finalItemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
-                    .forEach(player::sendMessage);
+            TextUtil
+                .prepareMessage(Triggers.PICKUP, originItemName, finalItemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
+                .forEach(player::sendMessage);
         }
     }
 
@@ -182,6 +206,44 @@ public class InventoryListener {
         }
     }
 
+    @Listener
+    public void onJoin(Join event, @First Player player) {
+        StreamSupport
+            .stream(player.getInventory().slots().spliterator(), false)
+            .filter(inventory -> inventory instanceof Slot)
+            .forEach(
+                inventory ->
+                    inventory
+                        .peek()
+                        .ifPresent(
+                            itemStack -> {
+                                CheckResult checkResult = service.check(itemStack, player.getWorld(), Triggers.JOIN, player);
+
+                                if (checkResult.isBanned()) {
+                                    checkResult
+                                        .getFinalView()
+                                        .map(dataContainer -> NbtTagDataUtil.toItemStack(dataContainer, itemStack.getQuantity()))
+                                        .ifPresent(
+                                            finalItem -> {
+                                                inventory.set(finalItem);
+                                                TextUtil
+                                                    .prepareMessage(
+                                                        Triggers.JOIN,
+                                                        TextUtil.getDisplayName(itemStack),
+                                                        TextUtil.getDisplayName(finalItem),
+                                                        ((CheckResult.Banned) checkResult).getBanRules(),
+                                                        checkResult.isUpdateNeeded()
+                                                    )
+                                                    .forEach(player::sendMessage);
+                                            }
+                                        );
+                                }
+                            }
+                        )
+            );
+    }
+
+    @IsCancelled(Tristate.FALSE)
     @Listener(order = Order.FIRST, beforeModifications = true)
     public void onInteractBlock(InteractBlockEvent event, @First Player player) {
         Optional<ItemStackSnapshot> optionalItem = event.getContext().get(EventContextKeys.USED_ITEM);
@@ -192,25 +254,30 @@ public class InventoryListener {
                 event.setCancelled(true);
             }
         }
-        if (!event.isCancelled()) {
-            CheckRuleTrigger trigger = Triggers.INTERACT;
-            checkInteractBlock(player, trigger, event.getTargetBlock());
-        }
+        CheckRuleTrigger trigger = Triggers.INTERACT;
+        checkInteractBlock(player, trigger, event.getTargetBlock());
     }
 
     private void checkInteractBlock(Player player, CheckRuleTrigger trigger, BlockSnapshot snapshot) {
         CheckResult result = service.check(snapshot, player.getWorld(), trigger, player);
         if (result.isBanned()) {
-            Optional<BlockSnapshot> optionalFinalBlock = result.getFinalView().map(view -> NbtTagDataUtil.toBlockSnapshot(view, snapshot.getWorldUniqueId()));
+            Optional<BlockSnapshot> optionalFinalBlock = result
+                .getFinalView()
+                .map(view -> NbtTagDataUtil.toBlockSnapshot(view, snapshot.getWorldUniqueId()));
             optionalFinalBlock.ifPresent(blockSnapshot -> blockSnapshot.restore(true, BlockChangeFlags.NONE));
             Text originName = Text.of(snapshot.getState().getType().getTranslation());
             Text finalName = Text.of(optionalFinalBlock.orElse(snapshot).getState().getType().getTranslation());
-            TextUtil.prepareMessage(trigger, originName, finalName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
-                    .forEach(player::sendMessage);
-            result.getFinalView().ifPresent(view -> {
-                UUID worldUniqueId = snapshot.getWorldUniqueId();
-                NbtTagDataUtil.toBlockSnapshot(view, worldUniqueId).restore(true, BlockChangeFlags.NONE);
-            });
+            TextUtil
+                .prepareMessage(trigger, originName, finalName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
+                .forEach(player::sendMessage);
+            result
+                .getFinalView()
+                .ifPresent(
+                    view -> {
+                        UUID worldUniqueId = snapshot.getWorldUniqueId();
+                        NbtTagDataUtil.toBlockSnapshot(view, worldUniqueId).restore(true, BlockChangeFlags.NONE);
+                    }
+                );
         }
     }
 
@@ -221,32 +288,43 @@ public class InventoryListener {
             finalItem.ifPresent(itemStack -> player.setItemInHand(handType, itemStack));
             Text originName = getDisplayName(item.createStack());
             Text finalItemName = getDisplayName(finalItem.orElse(item.createStack()));
-            TextUtil.prepareMessage(trigger, originName, finalItemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
-                    .forEach(player::sendMessage);
+            TextUtil
+                .prepareMessage(trigger, originName, finalItemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
+                .forEach(player::sendMessage);
             return true;
         }
         return false;
     }
 
     private boolean checkInventory(Player player, CheckRuleTrigger trigger, Stream<? extends Transaction<ItemStackSnapshot>> trans) {
-        return trans.anyMatch(tran -> {
-            ItemStackSnapshot item = tran.getFinal();
-            CheckResult result = service.check(item, player.getWorld(), trigger, player);
-            if (result.isBanned()) {
-                Optional<DataContainer> viewOptional = result.getFinalView();
-                if (viewOptional.isPresent()) {
-                    ItemStack finalItem = getItem(viewOptional.get(), item.getQuantity());
-                    TextUtil.prepareMessage(trigger, getDisplayName(item.createStack()), getDisplayName(finalItem), ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
+        return trans.anyMatch(
+            tran -> {
+                ItemStackSnapshot item = tran.getFinal();
+                CheckResult result = service.check(item, player.getWorld(), trigger, player);
+                if (result.isBanned()) {
+                    Optional<DataContainer> viewOptional = result.getFinalView();
+                    if (viewOptional.isPresent()) {
+                        ItemStack finalItem = getItem(viewOptional.get(), item.getQuantity());
+                        TextUtil
+                            .prepareMessage(
+                                trigger,
+                                getDisplayName(item.createStack()),
+                                getDisplayName(finalItem),
+                                ((CheckResult.Banned) result).getBanRules(),
+                                result.isUpdateNeeded()
+                            )
                             .forEach(player::sendMessage);
-                    tran.setCustom(finalItem.createSnapshot());
-                } else {
-                    Text itemName = getDisplayName(item.createStack());
-                    TextUtil.prepareMessage(trigger, itemName, itemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded()).forEach(player::sendMessage);
-                    return true;
+                        tran.setCustom(finalItem.createSnapshot());
+                    } else {
+                        Text itemName = getDisplayName(item.createStack());
+                        TextUtil
+                            .prepareMessage(trigger, itemName, itemName, ((CheckResult.Banned) result).getBanRules(), result.isUpdateNeeded())
+                            .forEach(player::sendMessage);
+                        return true;
+                    }
                 }
+                return false;
             }
-            return false;
-        });
+        );
     }
-
 }
