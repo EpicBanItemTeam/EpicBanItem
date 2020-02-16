ad8c8bc975844cfccc88535619ece6c0975105d7
GiNYAi <ginyai@qq.com>
2020-02-16 23:57:28 +0800

    fix cannot help child command

--- src/main/java/com/github/euonmyoji/epicbanitem/command/CommandHelp.java
+++ src/main/java/com/github/euonmyoji/epicbanitem/command/CommandHelp.java
@@ -31,7 +31,7 @@ public class CommandHelp extends AbstractCommand {
 
     @Override
     public CommandElement getArgument() {
-        return GenericArguments.optional(GenericArguments.choices(Text.of("sub-command"), service.getFlatMap(), false, false));
+        return GenericArguments.optional(GenericArguments.choices(Text.of("sub-command"), service.getFlatMap()::keySet, service.getFlatMap()::get, false));
     }
 
     @Override
