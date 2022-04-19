277a129120ccdad87b8c9e9ef4eb7c2448b4fe1d
SettingDust <settingdust@gmail.com>
2022-04-19 18:15:44 +0800

    feat: use methods instead of lambda for events
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/team/ebi/epicbanitem/EBIRegistries.java
+++ src/main/java/team/ebi/epicbanitem/EBIRegistries.java
@@ -2,10 +2,9 @@ package team.ebi.epicbanitem;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import io.leangen.geantyref.TypeToken;
 import org.spongepowered.api.Server;
-import org.spongepowered.api.event.EventListenerRegistration;
 import org.spongepowered.api.event.EventManager;
+import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.lifecycle.RegisterRegistryEvent;
 import org.spongepowered.api.event.lifecycle.RegisterRegistryValueEvent;
@@ -24,7 +23,13 @@ public class EBIRegistries {
   public static RegistryType<QueryExpressionFunction> QUERY_EXPRESSION;
   public static RegistryType<UpdateExpressionFunction> UPDATE_EXPRESSION;
 
-  public static void registerRegistries(final RegisterRegistryEvent event) {
+  @Inject
+  public EBIRegistries(PluginContainer pluginContainer, EventManager eventManager) {
+    eventManager.registerListeners(pluginContainer, this);
+  }
+
+  @Listener
+  public void onRegisterRegistry(RegisterRegistryEvent.EngineScoped<Server> event) {
     TRIGGER = event.register(EpicBanItem.key("trigger"), false, () -> Triggers.DEFAULT_REGISTRIES);
 
     QUERY_EXPRESSION =
@@ -38,25 +43,9 @@ public class EBIRegistries {
             () -> UpdateExpressions.DEFAULT_REGISTRIES);
   }
 
-  @Inject
-  public EBIRegistries(PluginContainer pluginContainer, EventManager eventManager) {
-    eventManager.registerListener(
-        EventListenerRegistration.builder(
-                new TypeToken<RegisterRegistryEvent.EngineScoped<Server>>() {})
-            .plugin(pluginContainer)
-            .listener(EBIRegistries::registerRegistries)
-            .build());
-
-    eventManager.registerListener(
-        EventListenerRegistration.builder(
-                new TypeToken<RegisterRegistryValueEvent.EngineScoped<Server>>() {})
-            .plugin(pluginContainer)
-            .order(Order.POST)
-            .listener(
-                event -> {
-                  QueryExpressions.EXPRESSIONS = QueryExpressions.toMap();
-                  UpdateExpressions.EXPRESSIONS = UpdateExpressions.toMap();
-                })
-            .build());
+  @Listener(order = Order.POST)
+  public void onRegisterRegistryValue(RegisterRegistryValueEvent.EngineScoped<Server> event) {
+    QueryExpressions.EXPRESSIONS = QueryExpressions.toMap();
+    UpdateExpressions.EXPRESSIONS = UpdateExpressions.toMap();
   }
 }
