e530ccd9cd41dc03af8820b74a2be06b540836fd
SettingDust <settingdust@gmail.com>
2022-05-01 13:11:39 +0800

    chore: expression registry shouldn't contain $
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/team/ebi/epicbanitem/api/expression/ExpressionKeys.java
+++ src/main/java/team/ebi/epicbanitem/api/expression/ExpressionKeys.java
@@ -2,44 +2,49 @@ package team.ebi.epicbanitem.api.expression;
 
 import com.google.common.collect.Sets;
 import java.util.Set;
+import org.spongepowered.api.data.persistence.DataQuery;
 
 public class ExpressionKeys {
   // Query
-  public static final String OR = "$or";
-  public static final String NOR = "$nor";
-  public static final String AND = "$and";
-  public static final String NOT = "$not";
+  public static final String OR = "or";
+  public static final String NOR = "nor";
+  public static final String AND = "and";
+  public static final String NOT = "not";
 
-  public static final String EQ = "$eq";
-  public static final String NE = "$ne";
+  public static final String EQ = "eq";
+  public static final String NE = "ne";
 
-  public static final String GT = "$gt";
-  public static final String LT = "$lt";
-  public static final String GTE = "$gte";
-  public static final String LTE = "$lte";
+  public static final String GT = "gt";
+  public static final String LT = "lt";
+  public static final String GTE = "gte";
+  public static final String LTE = "lte";
 
-  public static final String IN = "$in";
-  public static final String NIN = "$nin";
+  public static final String IN = "in";
+  public static final String NIN = "nin";
 
-  public static final String SIZE = "$size";
-  public static final String ALL = "$all";
-  public static final String ELEM_MATCH = "$elemMatch";
+  public static final String SIZE = "size";
+  public static final String ALL = "all";
+  public static final String ELEM_MATCH = "elemMatch";
 
-  public static final String EXISTS = "$exists";
-  public static final String REGEX = "$regex";
+  public static final String EXISTS = "exists";
+  public static final String REGEX = "regex";
 
   // Update
-  public static final String SET = "$set";
-  public static final String UNSET = "$unset";
-  public static final String RENAME = "$rename";
-  public static final String POP = "$pop";
-  public static final String PULL = "$pull";
-  public static final String INC = "$ind";
-  public static final String MUL = "$mul";
+  public static final String SET = "set";
+  public static final String UNSET = "unset";
+  public static final String RENAME = "rename";
+  public static final String POP = "pop";
+  public static final String PULL = "pull";
+  public static final String INC = "ind";
+  public static final String MUL = "mul";
 
   /** The expressions can at root level */
   public static final Set<String> ROOT_QUERY_EXPRESSIONS = Sets.newHashSet(OR, NOR, AND);
 
   public static final Set<String> UPDATE_EXPRESSIONS =
       Sets.newHashSet(SET, UNSET, RENAME, POP, PULL, INC, MUL);
+
+  public static DataQuery query(String key) {
+    return DataQuery.of("$" + key);
+  }
 }
--- src/main/java/team/ebi/epicbanitem/api/expression/ExpressionQueries.java
+++ src/main/java/team/ebi/epicbanitem/api/expression/ExpressionQueries.java
@@ -3,37 +3,37 @@ package team.ebi.epicbanitem.api.expression;
 import org.spongepowered.api.data.persistence.DataQuery;
 
 public class ExpressionQueries {
-  public static final DataQuery OR = DataQuery.of(ExpressionKeys.OR);
-  public static final DataQuery NOR = DataQuery.of(ExpressionKeys.NOR);
-  public static final DataQuery AND = DataQuery.of(ExpressionKeys.AND);
-  public static final DataQuery NOT = DataQuery.of(ExpressionKeys.NOT);
+  public static final DataQuery OR = ExpressionKeys.query(ExpressionKeys.OR);
+  public static final DataQuery NOR = ExpressionKeys.query(ExpressionKeys.NOR);
+  public static final DataQuery AND = ExpressionKeys.query(ExpressionKeys.AND);
+  public static final DataQuery NOT = ExpressionKeys.query(ExpressionKeys.NOT);
 
-  public static final DataQuery EQ = DataQuery.of(ExpressionKeys.EQ);
-  public static final DataQuery NE = DataQuery.of(ExpressionKeys.NE);
+  public static final DataQuery EQ = ExpressionKeys.query(ExpressionKeys.EQ);
+  public static final DataQuery NE = ExpressionKeys.query(ExpressionKeys.NE);
 
-  public static final DataQuery GT = DataQuery.of(ExpressionKeys.GT);
-  public static final DataQuery LT = DataQuery.of(ExpressionKeys.LT);
-  public static final DataQuery GTE = DataQuery.of(ExpressionKeys.GTE);
-  public static final DataQuery LTE = DataQuery.of(ExpressionKeys.LTE);
+  public static final DataQuery GT = ExpressionKeys.query(ExpressionKeys.GT);
+  public static final DataQuery LT = ExpressionKeys.query(ExpressionKeys.LT);
+  public static final DataQuery GTE = ExpressionKeys.query(ExpressionKeys.GTE);
+  public static final DataQuery LTE = ExpressionKeys.query(ExpressionKeys.LTE);
 
-  public static final DataQuery IN = DataQuery.of(ExpressionKeys.IN);
-  public static final DataQuery NIN = DataQuery.of(ExpressionKeys.NIN);
+  public static final DataQuery IN = ExpressionKeys.query(ExpressionKeys.IN);
+  public static final DataQuery NIN = ExpressionKeys.query(ExpressionKeys.NIN);
 
-  public static final DataQuery SIZE = DataQuery.of(ExpressionKeys.SIZE);
-  public static final DataQuery ALL = DataQuery.of(ExpressionKeys.ALL);
-  public static final DataQuery ELEM_MATCH = DataQuery.of(ExpressionKeys.ELEM_MATCH);
+  public static final DataQuery SIZE = ExpressionKeys.query(ExpressionKeys.SIZE);
+  public static final DataQuery ALL = ExpressionKeys.query(ExpressionKeys.ALL);
+  public static final DataQuery ELEM_MATCH = ExpressionKeys.query(ExpressionKeys.ELEM_MATCH);
 
-  public static final DataQuery EXISTS = DataQuery.of(ExpressionKeys.EXISTS);
-  public static final DataQuery REGEX = DataQuery.of(ExpressionKeys.REGEX);
+  public static final DataQuery EXISTS = ExpressionKeys.query(ExpressionKeys.EXISTS);
+  public static final DataQuery REGEX = ExpressionKeys.query(ExpressionKeys.REGEX);
 
   // Update
-  public static final DataQuery SET = DataQuery.of(ExpressionKeys.SET);
-  public static final DataQuery UNSET = DataQuery.of(ExpressionKeys.UNSET);
-  public static final DataQuery RENAME = DataQuery.of(ExpressionKeys.RENAME);
+  public static final DataQuery SET = ExpressionKeys.query(ExpressionKeys.SET);
+  public static final DataQuery UNSET = ExpressionKeys.query(ExpressionKeys.UNSET);
+  public static final DataQuery RENAME = ExpressionKeys.query(ExpressionKeys.RENAME);
 
-  public static final DataQuery POP = DataQuery.of(ExpressionKeys.POP);
-  public static final DataQuery PULL = DataQuery.of(ExpressionKeys.PULL);
+  public static final DataQuery POP = ExpressionKeys.query(ExpressionKeys.POP);
+  public static final DataQuery PULL = ExpressionKeys.query(ExpressionKeys.PULL);
 
-  public static final DataQuery INC = DataQuery.of(ExpressionKeys.INC);
-  public static final DataQuery MUL = DataQuery.of(ExpressionKeys.MUL);
+  public static final DataQuery INC = ExpressionKeys.query(ExpressionKeys.INC);
+  public static final DataQuery MUL = ExpressionKeys.query(ExpressionKeys.MUL);
 }
