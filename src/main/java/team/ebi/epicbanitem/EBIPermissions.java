b09a86ae5cf654bfd03463f8764aec1782f7103c
SettingDust <settingdust@gmail.com>
2022-05-01 13:10:09 +0800

    chore: remove EBIPermissions.java
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/team/ebi/epicbanitem/EBIPermissions.java
+++
@@ -1,9 +0,0 @@
-package team.ebi.epicbanitem;
-
-import team.ebi.epicbanitem.api.RestrictionRule;
-
-public final class EBIPermissions {
-  public static String bypass(RestrictionRule rule) {
-    return EpicBanItem.permission("bypass" + rule.key());
-  }
-}
