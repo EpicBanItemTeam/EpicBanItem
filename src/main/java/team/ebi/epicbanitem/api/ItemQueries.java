596b37985d693c62c308296f8b764e3accfdc7ce
SettingDust <settingdust@gmail.com>
2022-05-12 16:35:47 +0800

    feat: add ItemQueries
    Signed-off-by: SettingDust <settingdust@gmail.com>

---
+++ src/main/java/team/ebi/epicbanitem/api/ItemQueries.java
@@ -0,0 +1,8 @@
+package team.ebi.epicbanitem.api;
+
+import org.spongepowered.api.data.persistence.DataQuery;
+
+public final class ItemQueries {
+  public static final DataQuery UNSAFE_DAMAGE = DataQuery.of("UnsafeDamage");
+  public static final DataQuery ITEM_TYPE = DataQuery.of("ItemType");
+}
--- src/main/java/team/ebi/epicbanitem/api/expression/ExpressionService.java
+++ src/main/java/team/ebi/epicbanitem/api/expression/ExpressionService.java
@@ -3,33 +3,20 @@ package team.ebi.epicbanitem.api.expression;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.ImplementedBy;
 import java.util.List;
-import java.util.Optional;
 import net.kyori.adventure.text.Component;
 import org.spongepowered.api.data.persistence.DataContainer;
 import org.spongepowered.api.data.persistence.DataQuery;
 import org.spongepowered.api.data.persistence.DataView;
 import org.spongepowered.api.data.persistence.Queries;
+import team.ebi.epicbanitem.api.ItemQueries;
 import team.ebi.epicbanitem.expression.ExpressionServiceImpl;
 
 @ImplementedBy(ExpressionServiceImpl.class)
 public interface ExpressionService {
   ImmutableSet<DataQuery> IGNORED =
-      ImmutableSet.of(
-          Queries.CONTENT_VERSION,
-          Queries.WORLD_KEY,
-          DataQuery.of("UnsafeDamage"),
-          DataQuery.of("Position"),
-          DataQuery.of("BlockState"));
+      ImmutableSet.of(Queries.CONTENT_VERSION, Queries.WORLD_KEY, ItemQueries.UNSAFE_DAMAGE);
 
-  default Optional<QueryResult> query(QueryExpression expression, DataView view) {
-    return expression.query(view);
-  }
-
-  default UpdateOperation update(UpdateExpression expression, QueryResult result, DataView view) {
-    return expression.update(result, view);
-  }
-
-  List<Component> renderQuery(DataView view, QueryResult result);
+  List<Component> renderQueryResult(DataView view, QueryResult result);
 
   static DataView cleanup(DataView view) {
     DataContainer container = DataContainer.createNew();
