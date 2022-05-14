656c64a88aadb3c246540a4c647a0490b1ef0944
SettingDust <settingdust@gmail.com>
2022-05-14 18:35:44 +0800

    feat: add test command
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/team/ebi/epicbanitem/EBICommands.java
+++ src/main/java/team/ebi/epicbanitem/EBICommands.java
@@ -11,6 +11,7 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.google.common.base.Predicates;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.io.IOException;
 import java.text.MessageFormat;
 import java.time.Duration;
 import java.util.Comparator;
@@ -21,8 +22,10 @@ import java.util.UUID;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.TranslatableComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.apache.commons.lang3.StringUtils;
@@ -41,6 +44,7 @@ import org.spongepowered.api.command.parameter.CommandContext;
 import org.spongepowered.api.data.DataManager;
 import org.spongepowered.api.data.SerializableDataHolder;
 import org.spongepowered.api.data.persistence.DataContainer;
+import org.spongepowered.api.data.persistence.DataFormats;
 import org.spongepowered.api.data.persistence.DataSerializable;
 import org.spongepowered.api.data.persistence.DataView;
 import org.spongepowered.api.entity.living.player.Player;
@@ -48,14 +52,18 @@ import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.equipment.EquipmentType;
+import org.spongepowered.api.registry.RegistryTypes;
 import org.spongepowered.api.world.BlockChangeFlags;
 import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.server.ServerWorld;
 import team.ebi.epicbanitem.api.RestrictionPreset;
 import team.ebi.epicbanitem.api.RestrictionPresets;
 import team.ebi.epicbanitem.api.RestrictionRule;
-import team.ebi.epicbanitem.api.RestrictionRuleQueries;
 import team.ebi.epicbanitem.api.RestrictionRuleService;
-import team.ebi.epicbanitem.api.RestrictionRules;
+import team.ebi.epicbanitem.api.RestrictionService;
+import team.ebi.epicbanitem.api.RestrictionTrigger;
+import team.ebi.epicbanitem.api.RestrictionTriggers;
+import team.ebi.epicbanitem.api.RulePredicateService;
 import team.ebi.epicbanitem.api.expression.ExpressionQueries;
 import team.ebi.epicbanitem.api.expression.ExpressionService;
 import team.ebi.epicbanitem.api.expression.QueryExpression;
@@ -69,11 +77,14 @@ import team.ebi.epicbanitem.util.command.Flags;
 import team.ebi.epicbanitem.util.command.Parameters;
 
 @Singleton
-public final class EBICommand {
+public final class EBICommands {
   @Inject private ExpressionService expressionService;
   @Inject private RestrictionRuleService ruleService;
-
-  EBICommand() {}
+  @Inject private RestrictionService restrictionService;
+  @Inject private RulePredicateService predicateService;
+  @Inject private Parameters parameters;
+  @Inject private Parameters.Keys keys;
+  @Inject private Flags flags;
 
   public Command.Parameterized build() {
     final Command.Parameterized query =
@@ -82,8 +93,8 @@ public final class EBICommand {
             .extendedDescription(
                 Component.translatable("epicbanitem.command.query.description.extended"))
             .permission(EpicBanItem.permission("command.query"))
-            .addFlag(Flags.BLOCK)
-            .addParameter(Parameters.QUERY.optional().key(Parameters.Keys.QUERY).build())
+            .addFlag(flags.block)
+            .addParameter(parameters.query.optional().key(keys.query).build())
             .executor(this::query)
             .build();
 
@@ -91,7 +102,7 @@ public final class EBICommand {
         Command.builder()
             .shortDescription(Component.translatable("epicbanitem.command.remove.description"))
             .permission(EpicBanItem.permission("command.remove"))
-            .addParameter(Parameters.RULE_KEY.key(Parameters.Keys.RULE_KEY).build())
+            .addParameter(parameters.ruleKey.key(keys.ruleKey).build())
             .executor(this::remove)
             .build();
 
@@ -101,20 +112,32 @@ public final class EBICommand {
             .extendedDescription(
                 Component.translatable("epicbanitem.command.update.description.extended"))
             .permission(EpicBanItem.permission("command.update"))
-            .addFlag(Flags.BLOCK)
-            .addParameter(Parameters.UPDATE.key(Parameters.Keys.UPDATE).build())
+            .addFlag(flags.block)
+            .addParameter(parameters.update.key(keys.update).build())
             .executor(this::update)
             .build();
 
     final Command.Parameterized create =
         Command.builder()
             .shortDescription(Component.translatable("epicbanitem.command.create.description"))
+            .extendedDescription(
+                Component.translatable("epicbanitem.command.create.description.extended"))
             .permission(EpicBanItem.permission("command.create"))
-            .addFlags(Flags.PRESET, Flags.BLOCK)
+            .addFlags(flags.preset, flags.block)
             .addParameters(
-                Parameters.RULE_NAME.key(Parameters.Keys.RULE_NAME).build(),
-                Parameters.QUERY.key(Parameters.Keys.QUERY).optional().build())
+                parameters.ruleName.key(keys.ruleName).build(),
+                parameters.query.key(keys.query).optional().build())
             .executor(this::create)
+            .build();
+
+    final Command.Parameterized test =
+        Command.builder()
+            .shortDescription(Component.translatable("epicbanitem.command.test.description"))
+            .extendedDescription(
+                Component.translatable("epicbanitem.command.test.description.extended"))
+            .permission(EpicBanItem.permission("command.test"))
+            .addFlags(flags.block, flags.trigger, flags.world)
+            .executor(this::test)
             .build();
 
     return Command.builder()
@@ -124,24 +147,28 @@ public final class EBICommand {
         .addChild(remove, "remove", "rm")
         .addChild(update, "update")
         .addChild(create, "create")
+        .addChild(test, "test")
         .build();
   }
 
   private @NotNull CommandResult remove(final CommandContext context) {
-    ResourceKey key = context.requireOne(Parameters.Keys.RULE_KEY);
+    ResourceKey key = context.requireOne(keys.ruleKey);
     String stringKey = key.asString();
-    RestrictionRule rule = RestrictionRules.remove(key);
+    RestrictionRule rule = ruleService.remove(key);
     if (Objects.isNull(rule)) {
       TextComponent.Builder builder = Component.text();
       builder.append(Component.translatable("epicbanitem.command.remove.notExist"));
       //noinspection deprecation
-      RestrictionRules.keyStream()
+      ruleService
+          .keys()
           .map(ResourceKey::asString)
           .min(Comparator.comparingInt(k -> StringUtils.getLevenshteinDistance(k, stringKey)))
           .map(
               it ->
                   Component.translatable("epicbanitem.command.suggestRule")
-                      .args(Component.text(it).clickEvent(ClickEvent.copyToClipboard(it))))
+                      .args(
+                          Component.text(it)
+                              .clickEvent(ClickEvent.suggestCommand("ebi remove " + it))))
           .ifPresent(it -> builder.append(Component.newline()).append(it));
       return CommandResult.error(builder.build());
     }
@@ -161,7 +188,7 @@ public final class EBICommand {
     // Argument > Last > Empty
     RootQueryExpression expression =
         context
-            .one(Parameters.Keys.QUERY)
+            .one(keys.query)
             .orElse(Objects.requireNonNull(usedQuery.get(uuid, it -> new RootQueryExpression())));
     usedQuery.put(uuid, expression);
     SerializableDataHolder targetObject =
@@ -195,7 +222,7 @@ public final class EBICommand {
     // Last > Empty
     QueryExpression queryExpression =
         Objects.requireNonNull(usedQuery.get(uuid, it -> new RootQueryExpression()));
-    UpdateExpression updateExpression = context.requireOne(Parameters.Keys.UPDATE);
+    UpdateExpression updateExpression = context.requireOne(keys.update);
     ItemStack targetObject;
     EquipmentType hand = null;
     BlockSnapshot block = null;
@@ -247,21 +274,23 @@ public final class EBICommand {
       BlockSnapshot newSnapshot = BlockSnapshot.builder().from(block).blockState(newState).build();
       newSnapshot.restore(true, BlockChangeFlags.DEFAULT_PLACEMENT);
     } else player.equip(hand, deserialized);
-    player.sendMessage(operation.asComponent());
+    player.sendMessage(
+        Component.translatable("epicbanitem.command.update.result")
+            .color(NamedTextColor.GREEN)
+            .append(operation.asComponent()));
     return CommandResult.success();
   }
 
-  private CommandResult create(CommandContext context) {
+  private @NotNull CommandResult create(CommandContext context) {
     if (!(context.cause().root() instanceof ServerPlayer))
       return CommandResult.error(
           Component.translatable("epicbanitem.command.needPlayer", NamedTextColor.RED));
     final ServerPlayer player = (ServerPlayer) context.cause().root();
-    final RestrictionPreset preset =
-        context.one(Parameters.Keys.PRESET).orElse(RestrictionPresets.TYPE.get());
-    final ResourceKey name = context.requireOne(Parameters.Keys.RULE_NAME);
-    final Optional<RootQueryExpression> expression = context.one(Parameters.Keys.QUERY);
+    final RestrictionPreset preset = context.one(keys.preset).orElse(RestrictionPresets.TYPE.get());
+    final ResourceKey name = context.requireOne(keys.ruleName);
+    final Optional<RootQueryExpression> expression = context.one(keys.query);
     DataContainer expressionView = DataContainer.createNew();
-    final boolean isBlock = context.hasFlag(Flags.BLOCK);
+    final boolean isBlock = context.hasFlag(flags.block);
     expression.ifPresent(it -> it.view().values(false).forEach(expressionView::set));
     if (isBlock) {
       Optional<BlockSnapshot> block = targetBlock(player);
@@ -302,49 +331,85 @@ public final class EBICommand {
     if (expressionView.keys(false).isEmpty()) {
       return CommandResult.error(Component.translatable("epicbanitem.command.create.noExpression"));
     }
-    ruleService.register(
-        name,
-        new RestrictionRuleImpl(
-            DataContainer.createNew().set(RestrictionRuleQueries.QUERY, expressionView)));
-    player.sendMessage(
-        Component.translatable("epicbanitem.command.create.success")
-            .args(Component.text(name.value()))
-            .append(Component.space())
-            .append(
-                Components.EDIT
-                    .color(NamedTextColor.GRAY)
-                    .clickEvent(
-                        SpongeComponents.executeCallback(
-                            cause -> {
-                              try {
-                                Sponge.server()
-                                    .commandManager()
-                                    .process(
-                                        player,
-                                        MessageFormat.format(
-                                            "{0} edit {1}", EpicBanItem.NAMESPACE, name));
-                              } catch (CommandException e) {
-                                throw new RuntimeException(e);
-                              }
-                            })))
-            .append(Component.space())
-            .append(
-                Components.TEST_HELD
-                    .color(NamedTextColor.GRAY)
-                    .clickEvent(
-                        SpongeComponents.executeCallback(
-                            cause -> {
-                              try {
-                                Sponge.server()
-                                    .commandManager()
-                                    .process(
-                                        player,
-                                        MessageFormat.format(
-                                            "{0} test {1}", EpicBanItem.NAMESPACE, name));
-                              } catch (CommandException e) {
-                                throw new RuntimeException(e);
-                              }
-                            }))));
+    ruleService.register(name, new RestrictionRuleImpl(new RootQueryExpression(expressionView)));
+    try {
+      player.sendMessage(
+          Component.translatable("epicbanitem.command.create.success")
+              .args(
+                  Component.text(name.value())
+                      .hoverEvent(Component.text(DataFormats.JSON.get().write(expressionView))))
+              .append(Component.space())
+              .append(
+                  Components.EDIT
+                      .color(NamedTextColor.GRAY)
+                      .clickEvent(
+                          SpongeComponents.executeCallback(
+                              cause -> {
+                                try {
+                                  Sponge.server()
+                                      .commandManager()
+                                      .process(
+                                          player,
+                                          MessageFormat.format(
+                                              "{0} edit {1}", EpicBanItem.NAMESPACE, name));
+                                } catch (CommandException e) {
+                                  throw new RuntimeException(e);
+                                }
+                              }))));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return CommandResult.success();
+  }
+
+  private @NotNull CommandResult test(CommandContext context) throws CommandException {
+    if (!(context.cause().root() instanceof ServerPlayer))
+      return CommandResult.error(
+          Component.translatable("epicbanitem.command.needPlayer", NamedTextColor.RED));
+    final ServerPlayer player = (ServerPlayer) context.cause().root();
+    final ServerWorld world = context.one(keys.world).orElse(player.world());
+    final boolean isBlock = context.hasFlag(flags.block);
+    ItemStack targetObject =
+        targetObject(player, isBlock)
+            .orElseThrow(
+                () ->
+                    new CommandException(
+                        isBlock
+                            ? Component.translatable("epicbanitem.command.needBlock")
+                            : Component.translatable("epicbanitem.command.needItem")));
+    DataContainer targetView = targetObject.toContainer();
+    final Stream<? extends RestrictionTrigger> triggers =
+        context.hasAny(keys.trigger)
+            ? context.all(keys.trigger).stream()
+            : RestrictionTriggers.registry().stream();
+    TranslatableComponent.Builder component =
+        Component.translatable("epicbanitem.command.test.result").toBuilder();
+    predicateService
+        .rules(targetObject.type().key(RegistryTypes.ITEM_TYPE))
+        .forEach(
+            rule -> {
+              player.sendMessage(
+                  Component.translatable(
+                      "epicbanitem.command.test.result.rule", rule.asComponent()));
+              triggers
+                  .map(
+                      trigger ->
+                          restrictionService.restrict(rule, targetView, world, trigger, null))
+                  .filter(Optional::isPresent)
+                  .map(Optional::get)
+                  .forEach(
+                      operation -> {
+                        if (component.children().isEmpty()) {
+                          component
+                              .append(Component.space())
+                              .append(
+                                  Component.translatable(
+                                      "epicbanitem.command.test.result.rule.operations"));
+                        }
+                        component.append(Component.newline()).append(operation.asComponent());
+                      });
+            });
+    player.sendMessage(component);
     return CommandResult.success();
   }
 }
