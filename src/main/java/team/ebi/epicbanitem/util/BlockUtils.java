a9e322eaaf01ed3a72c28a64282a906dccb953b1
SettingDust <settingdust@gmail.com>
2022-05-24 16:01:04 +0800

    feat: add all minecraft blocks with custom item
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/team/ebi/epicbanitem/util/BlockUtils.java
+++ src/main/java/team/ebi/epicbanitem/util/BlockUtils.java
@@ -14,6 +14,8 @@ import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.data.type.PistonTypes;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.plugin.PluginManager;
@@ -23,7 +25,7 @@ import net.minecraft.world.level.block.FlowerPotBlock;
 
 public final class BlockUtils {
 
-    private static final Map<Predicate<BlockSnapshot>, Function<BlockSnapshot, ItemStack>> BLOCK_TO_ITEM =
+    private static final Map<Predicate<BlockSnapshot>, Function<BlockSnapshot, Optional<ItemStack>>> BLOCK_TO_ITEM =
             Maps.newHashMap();
 
     static {
@@ -31,22 +33,50 @@ public final class BlockUtils {
         if (pluginManager.plugin("minecraft").isPresent()) {
             BLOCK_TO_ITEM.put(
                     it -> it.state().type().equals(BlockTypes.TALL_SEAGRASS.get()),
-                    ignored -> ItemStack.of(ItemTypes.SEAGRASS.get()));
-            BLOCK_TO_ITEM.put(it -> it.state().type() instanceof FlowerPotBlock, block -> ((BlockType)
-                            ((FlowerPotBlock) block.state().type()).getContent())
+                    ignored -> Optional.ofNullable(ItemStack.of(ItemTypes.SEAGRASS.get())));
+
+            BLOCK_TO_ITEM.put(it -> it.state().type() instanceof FlowerPotBlock, block -> Optional.ofNullable(((BlockType)
+                    ((FlowerPotBlock) block.state().type()).getContent())
                     .item()
                     .map(ItemStack::of)
-                    .orElse(ItemStack.of(ItemTypes.FLOWER_POT)));
+                    .orElse(ItemStack.of(ItemTypes.FLOWER_POT))));
+
+            BLOCK_TO_ITEM.put(
+                    it -> it.state().type().equals(BlockTypes.BAMBOO_SAPLING.get()),
+                    ignored -> Optional.ofNullable(ItemStack.of(ItemTypes.BAMBOO.get())));
+            // GrowingPlantBodyBlock
+            BLOCK_TO_ITEM.put(
+                    it -> it.state().type().equals(BlockTypes.KELP_PLANT.get()),
+                    ignored -> Optional.ofNullable(ItemStack.of(ItemTypes.KELP.get())));
+            BLOCK_TO_ITEM.put(
+                    it -> it.state().type().equals(BlockTypes.TWISTING_VINES_PLANT.get()),
+                    ignored -> Optional.ofNullable(ItemStack.of(ItemTypes.TWISTING_VINES.get())));
+            BLOCK_TO_ITEM.put(
+                    it -> it.state().type().equals(BlockTypes.WEEPING_VINES_PLANT.get()),
+                    ignored -> Optional.ofNullable(ItemStack.of(ItemTypes.WEEPING_VINES.get())));
+
+            BLOCK_TO_ITEM.put(
+                    it -> it.state().type().equals(BlockTypes.PISTON_HEAD.get()),
+                    block -> block.state().get(Keys.PISTON_TYPE).map(it -> {
+                        if (it.equals(PistonTypes.NORMAL.get())) {
+                            return ItemStack.of(ItemTypes.PISTON);
+                        } else if (it.equals(PistonTypes.STICKY.get())) {
+                            return ItemStack.of(ItemTypes.STICKY_PISTON);
+                        } else return null;
+                    }));
         }
     }
 
-    private BlockUtils() {}
+    private BlockUtils() {
+    }
 
     public static Optional<ItemStack> fromBlock(BlockSnapshot snapshot) {
         try {
             return Optional.of(BLOCK_TO_ITEM.entrySet().stream()
                     .filter(it -> it.getKey().test(snapshot))
                     .map(it -> it.getValue().apply(snapshot))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
                     .findAny()
                     .orElseGet(() ->
                             ItemStack.builder().fromBlockSnapshot(snapshot).build()));
