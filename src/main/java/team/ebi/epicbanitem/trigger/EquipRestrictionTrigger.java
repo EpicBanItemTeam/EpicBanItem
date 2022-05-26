8815ff921766160981af51edfef78b7cb38ca30b
SettingDust <settingdust@gmail.com>
2022-05-26 18:20:39 +0800

    chore(trigger): disable `equip` until #69 fixed
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/team/ebi/epicbanitem/trigger/EquipRestrictionTrigger.java
+++ src/main/java/team/ebi/epicbanitem/trigger/EquipRestrictionTrigger.java
@@ -5,32 +5,11 @@
  */
 package team.ebi.epicbanitem.trigger;
 
-import java.util.Locale;
-import java.util.Optional;
-
-import org.spongepowered.api.data.Transaction;
-import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.entity.ChangeEntityEquipmentEvent;
-import org.spongepowered.api.event.filter.Getter;
-import org.spongepowered.api.event.filter.cause.Last;
-import org.spongepowered.api.item.inventory.Equipable;
-import org.spongepowered.api.item.inventory.ItemStackSnapshot;
-import org.spongepowered.api.registry.RegistryTypes;
-import org.spongepowered.api.service.permission.Subject;
-import org.spongepowered.api.util.locale.LocaleSource;
-import org.spongepowered.api.world.Locatable;
-
-import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.JoinConfiguration;
-import net.kyori.adventure.translation.GlobalTranslator;
 import team.ebi.epicbanitem.EpicBanItem;
 import team.ebi.epicbanitem.api.AbstractRestrictionTrigger;
 import team.ebi.epicbanitem.api.RestrictionService;
-import team.ebi.epicbanitem.api.rule.RestrictionRule;
 import team.ebi.epicbanitem.api.rule.RulePredicateService;
 
 @Singleton
@@ -46,45 +25,46 @@ public class EquipRestrictionTrigger extends AbstractRestrictionTrigger {
         super(EpicBanItem.key("equip"));
     }
 
-    @Listener
-    public void onChangeEntityEquipment(
-            final ChangeEntityEquipmentEvent event,
-            @Last Locatable locatable,
-            @Last Equipable equipable,
-            @Getter("transaction") Transaction<ItemStackSnapshot> transaction) {
-        final var item = transaction.finalReplacement();
-        if (item.isEmpty()) return;
-        final var itemStack = item.createStack();
-        final var subject = event.cause().last(Subject.class).orElse(null);
-        final var audience = event.cause().last(Audience.class);
-        final var localeSource = event.cause().last(LocaleSource.class);
-        final var locale = localeSource.map(LocaleSource::locale).orElse(Locale.getDefault());
-        final var container = item.toContainer();
-        final var itemType = item.type().key(RegistryTypes.ITEM_TYPE);
-        final var predicates = predicateService.predicates(itemType);
-        final var rules = predicateService
-                .rules(predicates)
-                .filter(it -> predicates.contains(it.predicate()))
-                .sorted(RulePredicateService.PRIORITY_ASC)
-                .toList();
-        final var components = Lists.<Component>newArrayList();
-        final var world = locatable.serverLocation().world();
-        for (RestrictionRule rule : rules) {
-            if (rule.needCancel()) {
-                event.setCancelled(true);
-                components.add(GlobalTranslator.render(rule.canceledMessage().args(rule, this, itemStack), locale));
-            }
-            Optional<ItemStackSnapshot> result = restrictionService
-                    .restrict(rule, container, world, this, subject)
-                    .flatMap(it -> restrictionService.processedItem(container, it));
-            if (result.isEmpty()) continue;
-            transaction.setCustom(result.get());
-            components.add(GlobalTranslator.render(
-                    rule.updatedMessage()
-                            .args(rule, this, itemStack, result.get().createStack()),
-                    locale));
-        }
-
-        audience.ifPresent(it -> it.sendMessage(Component.join(JoinConfiguration.newlines(), components)));
-    }
+    //    @Listener
+    //    public void onChangeEntityEquipment(
+    //            final ChangeEntityEquipmentEvent event,
+    //            @Last Locatable locatable,
+    //            @Last Equipable equipable,
+    //            @Getter("transaction") Transaction<ItemStackSnapshot> transaction) {
+    //        final var item = transaction.finalReplacement();
+    //        if (item.isEmpty()) return;
+    //        final var itemStack = item.createStack();
+    //        final var subject = event.cause().last(Subject.class).orElse(null);
+    //        final var audience = event.cause().last(Audience.class);
+    //        final var localeSource = event.cause().last(LocaleSource.class);
+    //        final var locale = localeSource.map(LocaleSource::locale).orElse(Locale.getDefault());
+    //        final var container = item.toContainer();
+    //        final var itemType = item.type().key(RegistryTypes.ITEM_TYPE);
+    //        final var predicates = predicateService.predicates(itemType);
+    //        final var rules = predicateService
+    //                .rules(predicates)
+    //                .filter(it -> predicates.contains(it.predicate()))
+    //                .sorted(RulePredicateService.PRIORITY_ASC)
+    //                .toList();
+    //        final var components = Lists.<Component>newArrayList();
+    //        final var world = locatable.serverLocation().world();
+    //        for (RestrictionRule rule : rules) {
+    //            if (rule.needCancel()) {
+    //                event.setCancelled(true);
+    //                components.add(GlobalTranslator.render(rule.canceledMessage().args(rule, this, itemStack),
+    // locale));
+    //            }
+    //            Optional<ItemStackSnapshot> result = restrictionService
+    //                    .restrict(rule, container, world, this, subject)
+    //                    .flatMap(it -> restrictionService.processedItem(container, it));
+    //            if (result.isEmpty()) continue;
+    //            transaction.setCustom(result.get());
+    //            components.add(GlobalTranslator.render(
+    //                    rule.updatedMessage()
+    //                            .args(rule, this, itemStack, result.get().createStack()),
+    //                    locale));
+    //        }
+    //
+    //        audience.ifPresent(it -> it.sendMessage(Component.join(JoinConfiguration.newlines(), components)));
+    //    }
 }
